6e7df3a92b6e2bcfa6b50f2777208af48a5f414f
Denis <d.igoschin.igoshin@gmail.com>
2021-08-29 10:01:13 +0300

    Initial dist subtree commit

---
+++ js/chunk-2d0dde2f.e155a49e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0dde2f"],{"82fe":function(n,e,c){"use strict";function d(n,e){return" 123 "}c.r(e);const o={};o.render=d;e["default"]=o}}]);
+//# sourceMappingURL=chunk-2d0dde2f.e155a49e.js.map
---
+++ precache-manifest.1d7bedcd7c73b1e5276cf5f9bbd649aa.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62ac9e3745745543172",
+    "url": "/vue-test-2/css/app.2442776e.css"
+  },
+  {
+    "revision": "8915599eff4b30f3f68d",
+    "url": "/vue-test-2/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "9152221661c66afed05cc5ec11731bc3",
+    "url": "/vue-test-2/index.html"
+  },
+  {
+    "revision": "d62ac9e3745745543172",
+    "url": "/vue-test-2/js/app.cf125ae7.js"
+  },
+  {
+    "revision": "4bbdaa939575cea1fc20",
+    "url": "/vue-test-2/js/chunk-2d0c1056.3dac6896.js"
+  },
+  {
+    "revision": "c9225ece144ec545b207",
+    "url": "/vue-test-2/js/chunk-2d0c9708.a3095f65.js"
+  },
+  {
+    "revision": "04ffb8ef811471871537",
+    "url": "/vue-test-2/js/chunk-2d0d05dc.d614382c.js"
+  },
+  {
+    "revision": "4c622794741b500644be",
+    "url": "/vue-test-2/js/chunk-2d0d7699.9780b107.js"
+  },
+  {
+    "revision": "a118abac3e69f7a4e650",
+    "url": "/vue-test-2/js/chunk-2d0dde2f.e155a49e.js"
+  },
+  {
+    "revision": "d667298d5f5137a250b5",
+    "url": "/vue-test-2/js/chunk-2d2086b7.87f9930b.js"
+  },
+  {
+    "revision": "8dffa4159334f4d42e62",
+    "url": "/vue-test-2/js/chunk-2d20fef9.6908f7e6.js"
+  },
+  {
+    "revision": "d289962f2e3c8eb92813",
+    "url": "/vue-test-2/js/chunk-2d217357.e67a6a42.js"
+  },
+  {
+    "revision": "29f20e0c33c966b0113a",
+    "url": "/vue-test-2/js/chunk-2d2259e5.1c90eee9.js"
+  },
+  {
+    "revision": "ed94aa666c6432ab405c",
+    "url": "/vue-test-2/js/chunk-53279185.945b73d4.js"
+  },
+  {
+    "revision": "8915599eff4b30f3f68d",
+    "url": "/vue-test-2/js/chunk-vendors.f9393d75.js"
+  },
+  {
+    "revision": "370f2e447519867be57463bbb2a62f51",
+    "url": "/vue-test-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-test-2/robots.txt"
+  }
+]);
